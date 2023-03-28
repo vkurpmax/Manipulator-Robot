b470f2cabaf9962daabe9417b0dbdc3256c901fb
vkurpmax <putu.herdy@gmail.com>
2023-03-28 11:43:08 +0800

    edit manipulator_001.py

--- Dynamixel SDK-Master Putu/Python/Putu/manipulator_001.py
+++ Dynamixel SDK-Master Putu/Python/Putu/manipulator_001.py
@@ -49,23 +49,23 @@ else:
 from dynamixel_sdk import *                    # Uses Dynamixel SDK library
 
 # Control table address
-ADDR_MX_TORQUE_ENABLE      = 64               # Control table address is different in Dynamixel model
-ADDR_MX_GOAL_POSITION      = 116
-ADDR_MX_PRESENT_POSITION   = 132
+ADDR_MX_TORQUE_ENABLE      = 400                # Control table address is different in Dynamixel model
+ADDR_MX_GOAL_POSITION      = 800
+ADDR_MX_PRESENT_POSITION   = 500
 
 # Protocol version
 PROTOCOL_VERSION            = 1.0               # See which protocol version is used in the Dynamixel
 
 # Default setting
 DXL_ID                      = 1                 # Dynamixel ID : 1
-BAUDRATE                    = 57600             # Dynamixel default baudrate : 57600
+BAUDRATE                    = 1000000             # Dynamixel default baudrate : 57600
 DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
                                                 # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
 
 TORQUE_ENABLE               = 1                 # Value for enabling the torque
 TORQUE_DISABLE              = 0                 # Value for disabling the torque
-DXL_MINIMUM_POSITION_VALUE  = 10           # Dynamixel will rotate between this value
-DXL_MAXIMUM_POSITION_VALUE  = 4000            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+DXL_MINIMUM_POSITION_VALUE  = 500           # Dynamixel will rotate between this value
+DXL_MAXIMUM_POSITION_VALUE  = 800            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
 DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
 
 index = 0
