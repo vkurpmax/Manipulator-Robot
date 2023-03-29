ae858074f09329740d3c4a965bab3ca414e0faf7
vkurpmax <putu.herdy@gmail.com>
2023-03-29 15:51:16 +0800

    upload SPP_001.py

---
+++ ScrewPicker-master putu/SPP_001.py
@@ -0,0 +1,50 @@
+from ax12 import ax12
+from servoHandler import servoHandler
+from time import sleep
+import time
+
+servos = ax12.Ax12()
+
+servoHandlerObj = [0]
+servoHandlerObj.append(servoHandler(1, 500, servos))
+servoHandlerObj.append(servoHandler(2, 500, servos))
+servoHandlerObj.append(servoHandler(3, 500, servos))
+servoHandlerObj.append(servoHandler(4, 500, servos))
+
+# - - - - - - - - - - - - - - - - 
+# - CHECK MOVE DEMANDED FLAGS - -
+# - - - - - - - - - - - - - - - -
+def checkMoveDemandedFlags(servoID):
+    controlBit = False
+    for i in range(len(servoID)):
+        if(servoHandlerObj[servoID[i]].moveDemanded):
+            return True
+    return False
+
+# - - - - - - - - - - - - - - - - 
+# - - -  MOVE THEM SERVOS   - - -
+# - - - - - - - - - - - - - - - -
+def moveThemServos(servoID, pos, speed):
+    if isinstance(servoID, list): 
+        for index, servo_id in enumerate(servoID):
+            servoHandlerObj[servo_id].moveSmooth(pos[index], speed[index])
+        while checkMoveDemandedFlags(servoID):
+            for i in servoID:
+                servoHandlerObj[i].update()
+            servos.action()
+        sleep(0.1)
+    else:
+        servoHandlerObj[servoID].moveSmooth(pos, speed)
+        while checkMoveDemandedFlags([servoID]):
+            servoHandlerObj[servoID].update()
+        servos.action()
+        sleep(0.1)
+
+# - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - 
+# - - - MAIN PROG START - - - - -
+# - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - 
+moveThemServos(1, 800, 200)
+moveThemServos(2, 300, 200)
+moveThemServos(3, 300, 200)
