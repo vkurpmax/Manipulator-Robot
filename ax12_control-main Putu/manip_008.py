dfd774038e14df0fa1de5cf5c25ff24fa88fd657
vkurpmax <putu.herdy@gmail.com>
2023-04-19 05:11:32 +0800

    update manip_008.py

--- ax12_control-main Putu/manip_008.py
+++ ax12_control-main Putu/manip_008.py
@@ -23,6 +23,19 @@ theta1 = 0.0
 c3 = 0.0
 s3 = 0.0
 s3a = 0.0
+theta3 = 0.0
+p1 = 0.0
+p2 = 0.0
+theta2a = 0.0
+theta2 = 0.0
+
+T1a = 0.0
+T2a = 0.0
+T3a = 0.0
+
+T1 = 0.0
+T2 = 0.0
+T3 = 0.0
 
 keypressed = ''
 
@@ -293,9 +306,11 @@ def InputKoordinat_Z():
 def Formulasi():
     global L1, L2, L3
     global h, Z
-    global theta1
-    global X, Y
-    global c3, s3, s3a
+    global X, Y, theta1
+    global c3, s3, s3a, theta3
+    global p1, p2, theta2a, theta2
+    global T1a, T2a, T3a
+    global T1, T2, T3
 
     L1 = 85     # mm
     L2 = 165    # mm
@@ -308,7 +323,20 @@ def Formulasi():
     c3 = (X*X + Y*Y + h*h - L2*L2 - L3*L3) / (2*L2*L3)  # dunno what is c3?
     s3 = -math.sqrt(1 - c3*c3)  # for down elbow
     s3a = math.sqrt(1 - c3*c3)  # for up elbow
-    
+    theta3 = math.atan2(s3, c3) # radian
+    p1 = (Y*L3*math.cos(theta3)+Y*L2) + (X*L3*math.sin(theta3))
+    p2 = (X*L3*math.cos(theta3)+X*L2) - (Y*L3*math.sin(theta3))
+    theta2a = math.atan2(p1, p2)    # radian
+    theta2 = 2*theta2a  # radian
+
+    T1a = theta1 * 180.0 / math.pi  # degree
+    T2a = theta2 * 180.0 / math.pi  # degree
+    T3a = theta3 * 180.0 / math.pi  # degree
+
+    T1 = (150 + T1a) * 3.41
+    T2 = (60 + T2a) * 3.41
+    T3 = (150 + T3a) * 3.41
+
 
 # - - - - - - - - - - - - - - - - 
 # - - - -  MAIN PROGRAM   - - - -
