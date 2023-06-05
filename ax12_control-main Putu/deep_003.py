03d6b6720da88b312757d7af88319b7f0d14a6e9
vkurpmax <putu.herdy@gmail.com>
2023-06-05 13:36:08 +0800

    update

--- ax12_control-main Putu/deep_003.py
+++ ax12_control-main Putu/deep_003.py
@@ -9,7 +9,15 @@ model = YOLO('best_002.pt')  # load a custom model
 
 # Predict with the model
 results = model(source='img0000.png',
-                conf=0.55, save=True,
+                conf=0.55,
+                save=True,
                 save_txt=True,
                 show=True,
                 show_labels=True)  # predict on an image
+
+for result in results:
+    boxes = result.boxes  # Boxes object for bbox outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    probs = result.probs  # Class probabilities for classification outputs
+    
+print(boxes.xyxy)
